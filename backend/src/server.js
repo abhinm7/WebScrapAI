7dbaf7c06a3b3f28ad60bdc0acfb650038ade09e
ABHIN M <abhin18m@gmail.com>
2025-12-19 13:21:22 +0530

    fix env configs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-require('dotenv').config();
 const router = require('./routes/taskRoute');
 
 app.use(express.json());

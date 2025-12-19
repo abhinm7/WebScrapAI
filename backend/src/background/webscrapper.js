4c63eafb7a35f1d4f01b53c345ec1ef7bda59d75
ABHIN M <abhin18m@gmail.com>
2025-12-19 22:01:03 +0530

    optimized for production build broswer

--- backend/src/background/webscrapper.js
+++ backend/src/background/webscrapper.js
@@ -7,10 +7,10 @@ const fetchWebContent = async (url) => {
 
         const browser = await puppeteer.launch({
             headless: 'new',
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--single-process','--no-zygote'],
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-gpu', '--single-process', '--no-zygote'],
         });
 
-        console.log('Chrome path:', puppeteer.executablePath());
+        console.log('Chrome path:', process.env.PUPPETEER_EXECUTABLE_PATH || puppeteer.executablePath());
 
 
         const page = await browser.newPage();
